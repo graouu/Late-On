6e39ded3724ec173120fcea28ea75afb2683c5bc
Graou <balthouu@gmail.com>
2026-02-19 15:48:28 +0100

    started working on movement and structure

---
+++ late-on/Scripts/Place.gd
@@ -0,0 +1,39 @@
+class_name Place extends Control
+
+signal go_to
+
+@export var place_name : String
+
+#Leave the Place
+@export var button_left : Button
+@export var place_left : String
+
+@export var button_right : Button
+@export var place_right : String
+
+@export var button_up : Button
+@export var place_up : String
+
+@export var button_down : Button
+@export var place_down : String
+
+func _ready() -> void:
+	var functions : Array[Callable] = [pressed_button_left,pressed_button_right,pressed_button_up,pressed_button_down]
+	var buttons : Array[Button] = [button_left, button_right, button_up, button_down]
+	var i := 0
+	while i < 4 :
+		if buttons[i]:
+			buttons[i].pressed.connect(functions[i])
+		i+=1
+
+func pressed_button_left(next_place : String):
+	go_to.emit(place_left)
+
+func pressed_button_right(next_place : String):
+	go_to.emit(place_right)
+	
+func pressed_button_up(next_place : String):
+	go_to.emit(place_up)
+	
+func pressed_button_down(next_place : String):
+	go_to.emit(place_down)
