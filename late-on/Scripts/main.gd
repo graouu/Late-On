c6847c67c593044f00c5835b2229cc58bfbff689
Graou <balthouu@gmail.com>
2026-02-24 12:17:42 +0100

    added black transitions

--- late-on/Scripts/main.gd
+++ late-on/Scripts/main.gd
@@ -6,6 +6,8 @@ extends Control
 @export var current_place : Place
 #Dictionnaire des endroits
 @export var place_dic : Dictionary[String, PackedScene]
+#Animateur de la texture noire pour la transition
+@export var black_anim : AnimationPlayer
 
 func _ready() -> void:
 	load_places()
@@ -23,9 +25,12 @@ func load_places():
 #Retire l'endroit actuel et le remplace par l'endroit 'place'
 func move(place : String):
 	
-	place_control.get_child(0).queue_free()
+	black_anim.play('transition')
+	await black_anim.animation_finished
 	
+	place_control.get_child(0).queue_free()
 	current_place = place_dic[place].instantiate()
 	place_control.add_child(current_place)
 	current_place.go_to.connect(move)
 	
+	black_anim.play('detransition')
