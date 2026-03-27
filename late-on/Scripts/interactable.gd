f4230ced376fb98b82616002f5f33f53d7facb5c
Graou <balthouu@gmail.com>
2026-03-27 14:25:34 +0100

    starting to form the main sequence

--- late-on/Scripts/interactable.gd
+++ late-on/Scripts/interactable.gd
@@ -3,17 +3,19 @@
 class_name Interactable extends TextureButton
 
 signal open_dialogue(dialogue_ar : Array[DialogueLine])
+signal open_puzzle(puzzle : String)
 enum Type {DIALOGUE, PUZZLE}
 
 @export var interaction_type : Type = 0
 
 #La ligne de dialogue de l'interactable
 @export var dialogue_array : Array[DialogueLine]
-@export var puzzle : PackedScene
+@export var puzzle : String
 
 func _on_pressed() -> void:
+	print(name, " has been pressed")
 	#Quand l'interactable est pressé, on envoie un signal pour jouer ce dialogue
 	if interaction_type == Type.DIALOGUE:
 		open_dialogue.emit(dialogue_array)
 	else:
-		get_tree().change_scene_to_packed(puzzle)
+		open_puzzle.emit(puzzle)
